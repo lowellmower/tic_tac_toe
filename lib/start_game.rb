45b6141ef933615c97f4561390c08783559052a5
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-24 23:22:03 -0400

    runner file

---
+++ lib/start_game.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require_relative 'game'
+require_relative 'models/board'
+require_relative 'models/player'
+require_relative 'models/computer'
+require_relative 'views/view'
+require_relative 'controllers/games_controller'
+
+# ARGV = gets.chomp
+# game = Game.new
+# game.start_game
+binding.pry
