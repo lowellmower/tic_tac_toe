60124435807ddaf113444e6c32f8548f72901c4d
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-24 23:20:15 -0400

    first run at board logic

--- lib/models/board.rb
+++ lib/models/board.rb
@@ -1,10 +1,24 @@
 class Board
 
-  attr_accessor :game_board
+  attr_accessor :game_board, :board, :winner
 
-  def initialize(args)
+  def initialize(args = {})
     args = defaults.merge(args)
-    @game_board = prep_board(args[:game_board])
+    @game_board = args[:game_board]
+    @board = prep_board(@game_board)
+    @winner = nil
+  end
+
+  def move_piece(move, piece)
+    @game_board[move] = piece
+    @board = prep_board(@game_board)
+    # remove messaging and abstract into controller
+    p "quality move"
+  end
+
+  def reject_piece
+    # should return nil and propmt controller to communicate invalid move
+    p "sorry friend"
   end
 
   def winner?
@@ -16,34 +30,47 @@ class Board
     false
   end
 
+  def to_s
+    @board.map {|row| row.join(' | ') }.join("\n----------\n")
+  end
+
   private
   # refactor this
     def prep_board(board)
-      prepared_board = []
+      matrix_board = []
       board.each_slice(3) do |row|
-        prepared_board << row
+        matrix_board << row
       end
-      prepared_board
+      matrix_board
     end
 
     def col_check
-      @game_board.transpose.each do |col|
+      @board.transpose.each do |col|
+        @winner = col.uniq[0]
         return true if col.uniq.length == 1
       end
       false
     end
 
     def row_check
-      @game_board.each do |row|
+      @board.each do |row|
+        @winner = row.uniq[0]
         return true if row.uniq.length == 1
       end
       false
     end
 
     def diagonal_check
-      left_right_diag = (0..2).collect {|i| @game_board[i][i]}
-      right_left_diag = (0..2).collect {|i| @game_board.reverse[i][i]}
-      return true if left_right_diag.uniq.length == 1 || right_left_diag.uniq.length == 1
+      # refactor this
+      left_right_diag = (0..2).collect {|i| @board[i][i]}
+      right_left_diag = (0..2).collect {|i| @board.reverse[i][i]}
+      if left_right_diag.uniq.length == 1
+        @winner = left_right_diag.uniq[0]
+        return true
+      elsif right_left_diag.uniq.length == 1
+        @winner = right_left_diag.uniq[0]
+        return true
+      end
       false
     end
 
