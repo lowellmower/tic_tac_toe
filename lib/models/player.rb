1bfb496f61021eb6f4bbc511f923281863f87c83
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-27 20:47:03 -0400

    move player to parent class

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -1,20 +1,5 @@
 class Player
 
-  attr_accessor :name, :piece
-
-  def initialize(args = {})
-    args = defaults.merge(args)
-    @name = args[:name]
-    @piece = args[:piece]
-  end
-
-  def make_move(move, board)
-    if valid_move?(move) && move_available?(move, board)
-      place_piece(move, board) and return true
-    end
-    board.reject_piece
-  end
-
   def valid_move?(move)
     return false if move == ("X" || "O")
     return false if move.to_i > 9 || move.to_i < 1
@@ -30,10 +15,6 @@ class Player
     def move_available?(move, board)
       return false unless board.game_board.include?(move.to_s)
       true
-    end
-
-    def defaults
-      {name: "Human Player", piece: "X"}
     end
 
 end
