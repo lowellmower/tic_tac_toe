f452ac557dbd83e3efffa24c727c1eed9abdbd32
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-27 20:47:32 -0400

    edit require statements

--- lib/start_game.rb
+++ lib/start_game.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require_relative 'models/board'
-require_relative 'models/player'
-require_relative 'models/computer'
+require_relative 'models/human_player'
+require_relative 'models/computer_player'
 require_relative 'views/view'
 require_relative 'controllers/games_controller'
 
