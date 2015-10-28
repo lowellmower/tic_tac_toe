df859f60899cf3d4a5b1c2bee6df57741260bd80
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-28 09:46:47 -0400

    add helper for fake standard input

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,23 @@
-$: << "../lib/"
-require_relative 'board'
-require_relative 'computer_player'
-require_relative 'view'
-require_relative 'games_controller'
-require_relative 'human_player'
+require_relative '../lib/models/computer_player'
+require_relative '../lib/models/human_player'
+require_relative '../lib/models/player'
+require_relative '../lib/models/board'
+require_relative '../lib/views/view'
+require_relative '../lib/controllers/games_controller'
+
+module Helpers
+  def fake_stdin(*args)
+    begin
+      $stdin = StringIO.new
+      $stdin.puts(args.shift) until args.empty?
+      $stdin.rewind
+      yield
+    ensure
+      $stdin = STDIN
+    end
+  end
+end
+
+RSpec.configure do |conf|
+  conf.include(Helpers)
+end
